306873e9b9a58a3e2f1ccab5ec4328d95c236b9d
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-12-07 22:38:32 -0300

    adicionado informações de dentro do primeiro container colorido

--- lib/widgets/container_de_info.dart
+++ lib/widgets/container_de_info.dart
@@ -8,9 +8,50 @@ class ContainerDeInfo extends StatelessWidget {
     return Container(
       height: 100,
       margin: const EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
         color: Theme.of(context).primaryColor,
         borderRadius: BorderRadius.circular(17),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(right: 16),
+            child: Icon(
+              Icons.notifications_none,
+              size: 48,
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Data da próxima atualização:'),
+                    Text(
+                      '6 de janeiro de 2023',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium!
+                          .copyWith(fontWeight: FontWeight.normal),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Você será notificado em 3 dias',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(fontWeight: FontWeight.normal),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
