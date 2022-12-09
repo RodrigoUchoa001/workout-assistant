b9311709df85ee6f32fcc4168eeb8f3392facdba
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-12-08 23:22:51 -0300

    criada funcionalidade de poder escolher a altura do container colorido

--- lib/widgets/container_de_info.dart
+++ lib/widgets/container_de_info.dart
@@ -4,18 +4,20 @@ class ContainerDeInfo extends StatelessWidget {
   final Color corDeFundo;
   final Widget conteudo;
   final double comprimento;
+  final double altura;
   const ContainerDeInfo(
       {super.key,
       required this.corDeFundo,
       required this.conteudo,
-      this.comprimento = 0});
+      this.comprimento = 0,
+      this.altura = 100});
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
 
     return Container(
-      height: 100,
+      height: altura,
       width: (comprimento == 0 ? width : comprimento),
       margin: const EdgeInsets.symmetric(horizontal: 16),
       padding: const EdgeInsets.all(16),
