4ba0d22633beec321da49b6167e597b889d3eb4b
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-12-09 23:41:03 -0300

    removido scaffold desnecessário

--- lib/screens/tela_inicial.dart
+++ lib/screens/tela_inicial.dart
@@ -8,75 +8,73 @@ class TelaInicial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 32),
-              child: Text(
-                'Olá Usuário!',
-                style: Theme.of(context).textTheme.titleMedium,
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32),
+            child: Text(
+              'Olá Usuário!',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          ContainerDeInfo(
+            // INICIO DO CONTAINER DE INFO EXIBINDO MSG DE ATUALIZAR DADOS:
+            // corDeFundo: Theme.of(context).focusColor,
+            // conteudo: ConteudoDoContainerDeInfoComIcone(
+            //   icone: Icons.edit,
+            //   titulo: 'Chegou a hora de atualizar os dados!',
+            //   subtitulo: '',
+            //   acao: TextButton(
+            //     onPressed: () {},
+            //     child: const Text('Inserir novos dados'),
+            //   ),
+            // ),
+            corDeFundo: Theme.of(context).primaryColor,
+            conteudo: ConteudoDoContainerDeInfoComIcone(
+              icone: Icons.notifications_none,
+              titulo: 'Data da próxima atualização:',
+              subtitulo: '6 de janeiro de 2023',
+              acao: Text(
+                'Você será notificado em 3 dias.',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(fontWeight: FontWeight.normal, fontSize: 14),
               ),
             ),
-            ContainerDeInfo(
-              // INICIO DO CONTAINER DE INFO EXIBINDO MSG DE ATUALIZAR DADOS:
-              // corDeFundo: Theme.of(context).focusColor,
-              // conteudo: ConteudoDoContainerDeInfoComIcone(
-              //   icone: Icons.edit,
-              //   titulo: 'Chegou a hora de atualizar os dados!',
-              //   subtitulo: '',
-              //   acao: TextButton(
-              //     onPressed: () {},
-              //     child: const Text('Inserir novos dados'),
-              //   ),
-              // ),
-              corDeFundo: Theme.of(context).primaryColor,
-              conteudo: ConteudoDoContainerDeInfoComIcone(
-                icone: Icons.notifications_none,
-                titulo: 'Data da próxima atualização:',
-                subtitulo: '6 de janeiro de 2023',
-                acao: Text(
-                  'Você será notificado em 3 dias.',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium!
-                      .copyWith(fontWeight: FontWeight.normal, fontSize: 14),
-                ),
-              ),
+          ),
+          //
+          //
+          const SizedBox(height: 24),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: const [
+                Text('Ultima atualização:'),
+              ],
             ),
-            //
-            //
-            const SizedBox(height: 24),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                children: const [
-                  Text('Ultima atualização:'),
-                ],
-              ),
+          ),
+          const ContainerDeExibicaoDeUltimasMedidas(),
+          //
+          const SizedBox(height: 24),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: const [
+                Text('Treinos de hoje:'),
+              ],
             ),
-            const ContainerDeExibicaoDeUltimasMedidas(),
-            //
-            const SizedBox(height: 24),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                children: const [
-                  Text('Treinos de hoje:'),
-                ],
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32),
+            child: Text(
+              'Em breve!',
+              style: Theme.of(context).textTheme.titleMedium,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 32),
-              child: Text(
-                'Em breve!',
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
