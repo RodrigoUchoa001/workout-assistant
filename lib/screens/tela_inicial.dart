de5525961effd9b3b14162147d737cd8045500f4
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-12-09 22:58:53 -0300

    removido center desnecessário e ajustado margem de um elemento

--- lib/screens/tela_inicial.dart
+++ lib/screens/tela_inicial.dart
@@ -9,60 +9,54 @@ class TelaInicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // TODO: REMOVER O CENTER
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 32),
-              child: Text(
-                'Olá Usuário!',
-                style: Theme.of(context).textTheme.titleMedium,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32),
+            child: Text(
+              'Olá Usuário!',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          ContainerDeInfo(
+            // INICIO DO CONTAINER DE INFO EXIBINDO MSG DE ATUALIZAR DADOS:
+            // corDeFundo: Theme.of(context).focusColor,
+            // conteudo: ConteudoDoContainerDeInfoComIcone(
+            //   icone: Icons.edit,
+            //   titulo: 'Chegou a hora de atualizar os dados!',
+            //   subtitulo: '',
+            //   acao: TextButton(
+            //     onPressed: () {},
+            //     child: const Text('Inserir novos dados'),
+            //   ),
+            // ),
+            corDeFundo: Theme.of(context).primaryColor,
+            conteudo: ConteudoDoContainerDeInfoComIcone(
+              icone: Icons.notifications_none,
+              titulo: 'Data da próxima atualização:',
+              subtitulo: '6 de janeiro de 2023',
+              acao: Text(
+                'Você será notificado em 3 dias.',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(fontWeight: FontWeight.normal, fontSize: 14),
               ),
             ),
-            ContainerDeInfo(
-              // INICIO DO CONTAINER DE INFO EXIBINDO MSG DE ATUALIZAR DADOS:
-              // corDeFundo: Theme.of(context).focusColor,
-              // conteudo: ConteudoDoContainerDeInfoComIcone(
-              //   icone: Icons.edit,
-              //   titulo: 'Chegou a hora de atualizar os dados!',
-              //   subtitulo: '',
-              //   acao: TextButton(
-              //     onPressed: () {},
-              //     child: const Text('Inserir novos dados'),
-              //   ),
-              // ),
-              corDeFundo: Theme.of(context).primaryColor,
-              conteudo: ConteudoDoContainerDeInfoComIcone(
-                icone: Icons.notifications_none,
-                titulo: 'Data da próxima atualização:',
-                subtitulo: '6 de janeiro de 2023',
-                acao: Text(
-                  'Você será notificado em 3 dias.',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium!
-                      .copyWith(fontWeight: FontWeight.normal, fontSize: 14),
-                ),
-              ),
-            ),
-            //
-            //
-            const SizedBox(height: 24),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Ultima atualização:'),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                ),
+          ),
+          //
+          //
+          const SizedBox(height: 24),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: const [
+                Text('Ultima atualização:'),
               ],
             ),
-            //
-            //
-            const ContainerDeExibicaoDeUltimasMedidas(),
-          ],
-        ),
+          ),
+          const ContainerDeExibicaoDeUltimasMedidas(),
+        ],
       ),
     );
   }
