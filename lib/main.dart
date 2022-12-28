3e26a5a353c39f85468ebf25e2779844fc58f90e
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-12-27 22:06:58 -0300

    usando shared preferences para verificar se já houve primeiro cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,16 @@ import 'package:isar/isar.dart';
 import 'package:measure_tracker/db/banco_de_dados.dart';
 import 'package:measure_tracker/db/collections/medida.dart';
 import 'package:measure_tracker/db/collections/medidas_do_mes.dart';
+import 'package:measure_tracker/models/msg_de_add_medida.dart';
 import 'package:measure_tracker/ui/change_providers/pagina_exibida_provider.dart';
 import 'package:measure_tracker/ui/change_providers/tema_provider.dart';
 import 'package:measure_tracker/ui/screens/tela_da_bottom_nav_bar.dart';
+import 'package:measure_tracker/ui/screens/tela_de_add_medida.dart';
 import 'package:provider/provider.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+bool? feitoPrimeiroCadastro;
 
 void main() async {
   // pegando instância do bd isar, logo a frente é passado pro provider de
@@ -18,6 +23,10 @@ void main() async {
     [MedidaSchema, MedidasDoMesSchema],
     directory: dir.path,
   );
+
+  // TODO: Se necessário, colocar prefs no provider.
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  feitoPrimeiroCadastro = prefs.getBool('feitoPrimeiroCadastro');
 
   runApp(MyApp(isar: isar));
 }
@@ -41,7 +50,9 @@ class MyApp extends StatelessWidget {
           theme: Temas.temaClaro,
           // darkTheme: Temas.temaEscuro,
           themeMode: Provider.of<TemaProvider>(context).modoDeTemaAtual,
-          home: const TelaDaBottomNavBar(),
+          home: feitoPrimeiroCadastro == true
+              ? const TelaDaBottomNavBar()
+              : TelaDeAddMedida(msgsDeMedidas: msgDeMedidasInicial),
         );
       },
     );
