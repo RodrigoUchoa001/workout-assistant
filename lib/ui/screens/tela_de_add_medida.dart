eeafa1534eb98dd9ad135b0feab136e7fd5a252b
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-12-27 22:29:41 -0300

    adicionado campo para personalizar width do campo de texto ao inserir uma medida

--- lib/ui/screens/tela_de_add_medida.dart
+++ lib/ui/screens/tela_de_add_medida.dart
@@ -53,7 +53,10 @@ class _TelaDeAddMedidaState extends State<TelaDeAddMedida> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   SizedBox(
-                    width: 100,
+                    width:
+                        widget.msgsDeMedidas[contadorDeMedida].campoDeTextoLargo
+                            ? MediaQuery.of(context).size.width - 100
+                            : 100,
                     child: TextFormField(
                       controller: valorcontroller,
                       keyboardType: TextInputType.number,
