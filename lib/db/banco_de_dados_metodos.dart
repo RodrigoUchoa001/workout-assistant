24b60ea2538a51bcb437f66d604d937ea590b746
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-12-30 23:56:52 -0300

    criado métodos de add no bd

--- lib/db/banco_de_dados_metodos.dart
+++ lib/db/banco_de_dados_metodos.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
+import 'package:measure_tracker/db/collections/medida.dart';
+import 'package:measure_tracker/db/collections/medidas_do_mes.dart';
+import 'package:measure_tracker/models/msg_de_add_medida.dart';
 // import 'package:measure_tracker/db/collections/medida.dart';
 // import 'package:measure_tracker/db/collections/medidas_do_mes.dart';
 // import 'package:path_provider/path_provider.dart';
@@ -17,5 +21,56 @@ class BancoDeDadosMetodos {
   //   return isar;
   // }
 
-  void adicionarMedida() {}
+  /// a lista que vem no parametro virá na seguinte ordem:
+  /// [data, braço esq, braço dir, peito, costas, barriga, cintura, bumbum,
+  /// coxa esq, coxa dir, panturrilha esq, panturrilha dir, peso]
+  void addCadaMedida(List<TextEditingController> controllers) async {
+    List<String> tiposDeMedida = [
+      'Braço Esquerdo',
+      'Braço Direito',
+      'Peito',
+      'Costas',
+      'Barriga',
+      'Cintura',
+      'Bumbum',
+      'Coxa Esquerda',
+      'Coxa Direita',
+      'Panturrilha Esquerda',
+      'Panturrilha Direita',
+      'Peso',
+    ];
+
+    addMedidasDoMes(controllers[0].text); //adicionou o mes da medida
+
+    for (int i = 1; i < controllers.length; i++) {
+      addMedida(
+        Medida()
+          ..dataDaMedida = controllers[0].text
+          ..tipoDeMedida = tiposDeMedida[i - 1]
+          ..valorDaMedida = double.parse(controllers[i].text)
+          ..unidadeDaMedida = msgDeMedidasDeCadaMes[i].unidadeDeMedida
+          ..mesDaMedida.value = medidasDoMesDaVez,
+      );
+    }
+    debugPrint('deu certo add no bd');
+  }
+
+  late MedidasDoMes medidasDoMesDaVez;
+  void addMedidasDoMes(String data) async {
+    final novasMedidasDoMes = MedidasDoMes()..dataDasMedidas = data;
+    medidasDoMesDaVez = novasMedidasDoMes;
+
+    await isar.writeTxn(() async {
+      await isar.medidasDoMes.put(novasMedidasDoMes);
+    });
+  }
+
+  /// inserir medida individualmente
+  void addMedida(Medida medida) async {
+    await isar.writeTxn(
+      () async {
+        await isar.medidas.put(medida);
+      },
+    );
+  }
 }
