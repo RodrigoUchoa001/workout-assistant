af3f573ae88bf439f5db3c2fa2b9dcc21bc6f94e
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-01-03 21:47:20 -0300

    usando funções com a nova construção do bd

--- lib/db/banco_de_dados_metodos.dart
+++ lib/db/banco_de_dados_metodos.dart
@@ -1,11 +1,8 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:measure_tracker/db/collections/medida.dart';
 import 'package:measure_tracker/db/collections/medidas_do_mes.dart';
 import 'package:measure_tracker/models/msg_de_add_medida.dart';
-// import 'package:measure_tracker/db/collections/medida.dart';
-// import 'package:measure_tracker/db/collections/medidas_do_mes.dart';
-// import 'package:path_provider/path_provider.dart';
 
 class BancoDeDadosMetodos {
   Isar isar;
@@ -14,7 +11,7 @@ class BancoDeDadosMetodos {
   /// a lista que vem no parametro virá na seguinte ordem:
   /// [data, braço esq, braço dir, peito, costas, barriga, cintura, bumbum,
   /// coxa esq, coxa dir, panturrilha esq, panturrilha dir, peso]
-  void addCadaMedida(List<TextEditingController> controllers) async {
+  void addCadaMedida(List<dynamic> controllers) async {
     List<String> tiposDeMedida = [
       'Braço Esquerdo',
       'Braço Direito',
@@ -30,22 +27,19 @@ class BancoDeDadosMetodos {
       'Peso',
     ];
 
-    addMedidasDoMes(controllers[0].text); //adicionou o mes da medida
+    addMedidasDoMes(controllers[0]); //adicionou o mes da medida
 
     for (int i = 1; i < controllers.length; i++) {
-      addMedida(
-        Medida()
-          ..dataDaMedida = controllers[0].text
-          ..tipoDeMedida = tiposDeMedida[i - 1]
-          ..valorDaMedida = double.parse(controllers[i].text)
-          ..unidadeDaMedida = msgDeMedidasDeCadaMes[i].unidadeDeMedida
-          ..mesDaMedida.value = medidasDoMesDaVez,
-      );
+      addMedida(Medida()
+        ..tipo = tiposDeMedida[i - 1]
+        ..valor = double.parse(controllers[i].text)
+        ..unidade = msgDeMedidasDeCadaMes[i].unidadeDeMedida);
     }
+    medidasDoMesDaVez.medidas.save();
   }
 
   late MedidasDoMes medidasDoMesDaVez;
-  void addMedidasDoMes(String data) async {
+  void addMedidasDoMes(DateTime data) async {
     final novasMedidasDoMes = MedidasDoMes()..dataDasMedidas = data;
     medidasDoMesDaVez = novasMedidasDoMes;
 
@@ -63,16 +57,21 @@ class BancoDeDadosMetodos {
     );
   }
 
-  getMedidasDoMes() async {
-    final getMedidasDoMes = await isar.medidasDoMes.where().findAll();
-    return getMedidasDoMes;
+  // MÉTODOS DE GET
+  //
+
+  Future<List<MedidasDoMes>> getMedidasDoMes() async {
+    // final getMedidasDoMes = await isar.medidasDoMes.where().findAll();
+    // return getMedidasDoMes;
+    final medidasDoMes = await isar.medidasDoMes.where().findAll();
+    return medidasDoMes;
   }
 
-  getMedidas(MedidasDoMes medidasDoMes) async {
-    final getMedidas = await isar.medidas
-        .filter()
-        .dataDaMedidaEqualTo(medidasDoMes.dataDasMedidas)
-        .findAll();
-    return getMedidas;
-  }
+  // getMedidas(MedidasDoMes medidasDoMes) async {
+  //   final getMedidas = await isar.medidas
+  //       .filter()
+  //       .dataDaMedidaEqualTo(medidasDoMes.dataDasMedidas)
+  //       .findAll();
+  //   return getMedidas;
+  // }
 }
