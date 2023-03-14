6b96302b73f10d73fba21382a88cd1aa4a244d77
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-03-13 23:27:13 -0300

    criada classe de controle de meses ainda não atualizados com novas medidas

---
+++ lib/utils/meses_ainda_nao_preenchidos_controller.dart
@@ -0,0 +1,36 @@
+// TODO: remover pacote jiffy
+import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
+
+/// Classe que controla a quantidade de meses que ainda não foram atualizados
+/// com novas medidas.
+class MesesAindaNaoPreenchidosController extends ChangeNotifier {
+  List<DateTime> mesesAAtualizar = [];
+
+  void setMesesAindaNaoPreenchidosController(DateTime dataDaUltimaAtualizacao) {
+    mesesAAtualizar.clear();
+    DateTime dataATestar = dataDaUltimaAtualizacao;
+
+    while (dataATestar.isBefore(DateTime.now())) {
+      mesesAAtualizar.add(dataATestar);
+
+      dataATestar = DateTime(
+        dataATestar.year,
+        dataATestar.month + 1,
+        dataATestar.day,
+      );
+    }
+
+    // pois queremos as datas na ordem decrescente
+    mesesAAtualizar = mesesAAtualizar.reversed.toList();
+    notifyListeners();
+  }
+
+  // List<DateTime> get getMesesAAtualizar {
+  //   if (mesesAAtualizar.isEmpty) {
+  //     return [];
+  //   } else {
+  //     return mesesAAtualizar;
+  //   }
+  // }
+}
